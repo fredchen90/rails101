f09009caca0791ba4a02a9f6b8b286a09c507788
Fred <fredchen90@techbang.com.tw>
2015-11-10 17:19:01 +0800

    Add sign in/out authentication

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -42,6 +44,7 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_param
     params.require(:group).permit(:title, :description)
   end
