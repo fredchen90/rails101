55ccb51f02c5b6db586a9958a5aa3be665bb4567
Fred <fredchen90@techbang.com.tw>
2015-11-12 16:26:07 +0800

    User 在建立 group 後自動成為 group 的一員

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,9 +5,15 @@ class Group < ActiveRecord::Base
 
   has_many :posts
   has_many :group_users
-  has_many :members, through: :group_user, source: :user
+  has_many :members, through: :group_users, source: :user
+
+  after_create :create_join
 
   def editable_by?(user)
     user && user == owner
+  end
+
+  def create_join
+    members << owner
   end
 end
