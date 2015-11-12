205a6bf03faa2ea0f9583e38762be42fc22e8375
Fred <fredchen90@techbang.com.tw>
2015-11-12 11:41:56 +0800

    Member of group can post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :find_group
+  before_action :member_required, only: [:new, :create]
 
   def new
     @post = @group.posts.new
@@ -43,5 +44,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+  def member_required
+    if !current_user.is_member_of?(@group)
+      redirect_to group_path(@group)
+    end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,11 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
-  has_many :posts
+
   belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  has_many :posts
+  has_many :group_users
+  has_many :members, through: :group_user, source: :user
 
   def editable_by?(user)
     user && user == owner
