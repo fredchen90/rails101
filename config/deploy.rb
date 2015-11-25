d49a41451156bb56d52a7a1a5465d16a77e19e53
Fred <fredchen90@techbang.com.tw>
2015-11-25 15:59:08 +0800

    set unicorn preload_app

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,13 +32,23 @@ set :log_level, :debug
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # linked_dirs: after git clone is finished, the folders would be copied from shared folder
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{
+  config/database.yml
+  config/secrets.yml
+}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # linked_dirs: after git clone is finished, the folders would be copied from shared folder
-set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
+set :linked_dirs, %w{
+  log
+  tmp/pids
+  tmp/cache
+  tmp/sockets
+  vendor/bundle
+  public/system
+}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -48,9 +58,5 @@ set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "tmp/pids", "tmp/ca
 
 
 after "deploy:publishing", "deploy:restart"
-namespace :deploy do
-  task :restart do
-    invoke "unicorn:reload"
-  end
-end
+after 'deploy:restart', 'unicorn:restart'
 
